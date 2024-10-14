ca4060b623fb1ce4e34221798283aa06ee0c63ea
durielbaal <michaelogwu1997@gmail.com>
2024-10-14 09:53:18 +0200

    Initialized project

---
+++ src/test/java/com/myke/studios/pokeapi/PokeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myke.studios.pokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
