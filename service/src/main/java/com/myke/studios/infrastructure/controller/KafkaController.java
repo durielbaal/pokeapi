ef1f740df0c3edd2dcc06166d61e95dad7f6b83f
durielbaal <michaelogwu1997@gmail.com>
2024-11-11 19:55:53 +0100

    10: optimized imports

--- service/src/main/java/com/myke/studios/infrastructure/controller/KafkaController.java
+++ service/src/main/java/com/myke/studios/infrastructure/controller/KafkaController.java
@@ -3,7 +3,6 @@ package com.myke.studios.infrastructure.controller;
 import com.myke.studios.abstracts.AbstractEvent;
 import com.myke.studios.config.AbstractKafkaController;
 import com.myke.studios.config.KafkaProducer;
-import com.myke.studios.pokemonevent.insert.PokemonInsertEvent;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
